f60bf9c10dc815818852e26746b88d38b8d38523
MarceloLima11 <marcelolima152021@gmail.com>
2023-11-06 13:16:00 -0300

    feat: html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,44 @@
 function App() {
   return (
     <>
+      <nav>
+        <h1>Marcelo</h1>
+        <a href="#">Sobre mim</a>
+        <a href="#">Onde eu trabalhei?</a>
+        <a href="#">Projetos</a>
+        <a href="#">Habilidades</a>
+      </nav>
+
+      <main>
+        <h2>Olá, sou Marcelo Lima</h2>
+        <p>Software Developer</p>
+        <p>Desenvolvedor de software com experiência desde 2020 em projetos pessoais e de produção. Habilidades
+          em C#, SQL/NoSQL, Golang, JavaScript, React, Docker e Git. Especializado em desenvolvimento de
+          funcionalidades e refatoração de sistemas legados para melhorar desempenho e manutenção.</p>
+
+        <button>Baixe meu CV</button>
+        <button>Contate-me!</button>
+      </main>
+
+      <section>
+        <h3>Onde eu trabalhei?</h3>
+        <article>
+          <p>Estágio Backend</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsa quae distinctio magni quisquam excepturi, iure impedit dolore repellendus atque ducimus porro laboriosam omnis voluptatem asperiores natus veritatis nam nisi?</p>
+        </article>
+        <article>
+          <p>Desenvolvedor de Software</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsa quae distinctio magni quisquam excepturi, iure impedit dolore repellendus atque ducimus porro laboriosam omnis voluptatem asperiores natus veritatis nam nisi?</p>
+        </article>
+      </section>
+
+      <section>
+        <h3>Meus Projetos</h3>
+      </section>
+
+      <section>
+        <h3>Habilidades</h3>
+      </section>
     </>
   )
 }
