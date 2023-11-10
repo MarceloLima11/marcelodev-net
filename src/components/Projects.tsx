77b28a20b6e9d5dab850aa53a21c5c20a77a530f
MarceloLima11 <marcelolima152021@gmail.com>
2023-11-10 14:48:45 -0300

    wip: project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,19 @@
+import project1 from '../images/fake_project.png';
+
+
 import styles from './Projects.module.css';
 
 function Projects() {
     return (
         <section className={styles.projects_main_container}>
-            <h3>Meus Projetos</h3>
+            <h3>Projetos</h3>
 
 
+            <div className={styles.projects_main_container_cards}>
+                <div className={styles.card}><img src={project1} alt="" /></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+            </div>
         </section>
     );
 }
