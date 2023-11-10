d691b4989e81e3be9d34f2320c76e0b3ad49002e
MarceloLima11 <marcelolima152021@gmail.com>
2023-11-10 11:03:34 -0300

    feat: hero tech cards

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ function App() {
 
       <Experience />
 
-      <Projects />
-
       <Skills />
+
+      <Projects />
     </div>
   )
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,9 +1,22 @@
 import styles from './Skills.module.css';
+import go_hero from '../images/go_hero.png';
+import js_hero from '../images/js_hero.png';
+import csharp_hero from '../images/csharp_hero.png';
+import sass_hero from '../images/sass_hero.png';
 
 function Skills() {
     return (
         <section className={styles.skills_main_container}>
             <h3>Habilidades</h3>
+
+            <div className={styles.skills_main_container_images}>
+                <div id={styles.golang_card} className={styles.card}> <img src={go_hero} alt="Golang em héroi de desenho animado" /></div>
+                <div id={styles.javascript_card} className={styles.card}> <img src={js_hero} alt="JavaScript em héroi de desenho animado" /></div>
+                <div id={styles.csharp_card} className={styles.card}><img src={csharp_hero} alt="C# em héroi de desenho animado" /></div>
+                <div id={styles.sass_card} className={styles.card}><img src={sass_hero} alt="Sass em héri de desenho animado" /></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+            </div>
         </section>
     );
 }
