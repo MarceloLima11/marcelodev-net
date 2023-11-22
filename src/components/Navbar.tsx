349de4b77ecaf53fef089287e21a6de26e069832
MarceloLima11 <marcelolima152021@gmail.com>
2023-11-22 11:17:24 -0300

    feat: change navbar to fixed minimalist

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,20 @@
-import team_work from '../images/navbar/team_work.png';
-import dev_with_cat from '../images/navbar/dev_with_cat.png';
-import ui_designing from '../images/navbar/ui_design.png';
-import thinking_real_solution from '../images/navbar/thinking_real_solution.png';
+import user from '../images/navbar/user.png';
+import work from '../images/navbar/work.png';
+import book from '../images/navbar/book.png';
+import project from '../images/navbar/settings.png';
 
 import styles from './Navbar.module.css';
 
 function Navbar() {
     return (
-        <nav>
-            <h1>Marcelo</h1>
-
-            <div className={styles.navbar_main_container}>
-                <div>
-                    <img
-                        src={dev_with_cat}
-                        alt="Jovem programador com um gato no colo." />
-                </div>
-
-                <div>
-                    <img
-                        src={ui_designing}
-                        alt="Ilustrador desenhando uma arte." />
-
-                </div>
-
-                <div>
-                    <img
-                        src={team_work}
-                        alt="Equipe de trabalhadores construindo um prédio quase finalizado." />
-                </div>
-
-                <div>
-                    <img
-                        src={thinking_real_solution}
-                        alt="Programador pensando em soluções ao lado de fora e de noite." />
-                </div>
+        <nav className={styles.nav_main_container}>
+            <input type="checkbox" />
+            <div className={styles.fab}></div>
+            <div className={styles.fac}>
+                <a href="#"><img src={user} alt="Sobre mim icone" /></a>
+                <a href="#"><img src={work} alt="Experiência icone" /></a>
+                <a href="#"><img src={book} alt="Habilidades icone" /></a>
+                <a href="#"><img src={project} alt="Projetos icone" /></a>
             </div>
         </nav>
     );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,24 +1,15 @@
-import project1 from '../images/fake_project.png';
-
-
 import styles from './Projects.module.css';
 
 function Projects() {
     return (
-        <section className={styles.projects_main_container}>
+        <section id={styles.projects_main_container}>
             <h3>Projetos</h3>
 
-
-            <div id={styles.main_screen}>
-            </div>
             <div className={styles.projects_main_container_cards}>
-                <div className={styles.card}><img src={project1} alt="" /></div>
                 <div className={styles.card}></div>
                 <div className={styles.card}></div>
                 <div className={styles.card}></div>
                 <div className={styles.card}></div>
-                <div className={styles.card}></div>
-
             </div>
         </section>
     );
